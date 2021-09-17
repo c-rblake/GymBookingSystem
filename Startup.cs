f14036c56e60b430066e4627436df98fcf371f7c
Christoper <c.blake@hotmail.se>
2021-09-17 12:19:22 +0200

    13. Scaffold Controller 14. Update Database

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,7 @@ namespace GymBookingSystem
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
